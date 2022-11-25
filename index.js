b41c1b199c83a67612d8e77733b4095327878e10
stellagitonga <stellanyamburagitonga@gmail.com>
2022-11-25 13:59:22 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Code your solution in this file!
+let d2= 42;
+let pickup;
+
+function distanceFromHqInBlocks(pickup){
+    if (pickup>d2)return pickup-d2
+    return d2-pickup
+} 
+function distanceFromHqInFeet (pickup){
+    return distanceFromHqInBlocks(pickup)*264
+}
+function distanceTravelledInFeet(d1,d2){
+    return distanceFromHqInFeet(d2)-distanceFromHqInFeet(d1)
+}
+function calculatesFarePrice(d1,d2){
+    let distance= distanceTravelledInFeet(d1,d2)
+if(distance<400){
+   return 0; 
+}else if(distance>400 && distance<2000){
+    return(distance-400)*0.02
+}else if(distance>2000 && distance<2500){
+    return 25;
+}else if(distance>2500){
+    return "cannot travel that far"
+}
+    
+}
